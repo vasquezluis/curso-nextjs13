f03d1ba606ca61b800a4a08dbb3fbaed93dd7663
vasquezluis <luivasquez95@gmail.com>
2023-04-11 09:29:27 -0600

    data fetching added

--- first-nextjs-app/app/page.jsx
+++ first-nextjs-app/app/page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+const fecthUsers = async () => {
+  const res = await fetch('https://reqres.in/api/users')
+  const data = await res.json()
 
-function IndexPage () {
+  return data.data
+}
+
+async function IndexPage () {
+  const users = await fecthUsers()
+
   return (
     <div>
       <h1>Index page</h1>
+
+      <div>
+        {JSON.stringify(users)}
+      </div>
     </div>
   )
 }
