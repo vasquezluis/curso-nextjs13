bbe752b94376efc7aee03fa929d0ecb11e341dc3
vasquezluis <luivasquez95@gmail.com>
2023-04-11 10:24:05 -0600

    data fetching for individual user added

--- first-nextjs-app/app/users/[id]/page.jsx
+++ first-nextjs-app/app/users/[id]/page.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+const getUser = async (id) => {
+  const res = await fetch(`https://reqres.in/api/users/${id}`)
+  const data = await res.json()
 
-function usersPage () {
+  return data.data
+}
+
+async function usersPage ({ params }) {
+  const user = await getUser(params.id)
+
   return (
-    <div> user detail </div>
+    <div>
+
+      <h1>User details</h1>
+
+      <div>
+        <img src={user.avatar} alt={user.first_name} />
+
+        <div>
+          <h3>{user.id} {user.first_name} {user.last_name}</h3>
+          <p>{user.email}</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
