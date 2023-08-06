c9d8720392f2e48721a16f83ec508ca46403ab70
vasquezluis <luivasquez95@gmail.com>
2023-08-05 21:12:19 -0600

    Add database connection
    + Add mongodb connection using mongoose and
      Mongo Atlas

--- nextjs-mongodb-crud/src/app/api/ping/route.js
+++ nextjs-mongodb-crud/src/app/api/ping/route.js
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
 
+import { connectDB } from "@/utils/mongoose";
+
 export function GET(){
+
+  connectDB()
+
   return NextResponse.json({
     message: 'Hello world!'
   })
---
+++ nextjs-mongodb-crud/src/utils/mongoose.js
@@ -0,0 +1,24 @@
+import {connect, connection} from "mongoose";
+
+const conn = {
+  isConnected: false
+}
+
+export async function connectDB() {
+
+  if (conn.isConnected) return
+
+  const db = await connect("mongodb+srv://lvasquez0127:30889567@nextjs-crud-cluster.4z7r3g6.mongodb.net/?retryWrites=true&w=majority");
+  console.log(db.connection.db.databaseName)
+
+  conn.isConnected = db.connections[0].readyState
+
+}
+
+connection.on('connected', () => {
+  console.log('Mongodb is connected')
+})
+
+connection.on('error', () => {
+  console.log('Mongoose connection error')
+})
